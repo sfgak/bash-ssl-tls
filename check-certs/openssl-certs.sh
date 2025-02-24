2bfa1454fc1fa6b7f78653ae9c837f819347b5f5
Garafiev Aydar <garafievak@zabelka.dengimigom24.ru>
2025-02-24 11:46:37 +0300

    Showing certificates's issuer and subject added

--- check-certs/openssl-certs.sh
+++ check-certs/openssl-certs.sh
@@ -25,66 +25,76 @@ echo "Local IP address is $localip"
 
 parse_date() {
 	if [[ -n "$1" ]]; then
-                        month=$(echo $1 | sed 's/notAfter=//' | awk '{print $1}')
-                        year=$(echo $1 | sed 's/notAfter=//' | awk '{print $4}')
-                        day=$(echo $1 | sed 's/notAfter=//' | awk '{print $2}')
-                        case $month in
-                                Jan)
-                                        mn="01"
-                                        ;;
-                                Feb)
-                                        mn="02"
-                                        ;;
-                                Mar)
-                                        mn="03"
-                                        ;;
-                                Apr)
-                                        mn="04"
-                                        ;;
-                                May)
-                                        mn="05"
-                                        ;;
-                                Jun)
-                                        mn="06"
-                                        ;;
-                                Jul)
-                                        mn="07"
-                                        ;;
-                                Aug)
-                                        mn="08"
-                                        ;;
-                                Sep)
-                                        mn="09"
-                                        ;;
-                                Oct)
-                                        mn="10"
-                                        ;;
-                                Nov)
-                                        mn="11"
-                                        ;;
-                                Dec)
-                                        mn="12"
-                                        ;;
-                        esac
-                        certexpirydate="$year-$mn-$day"
-                        tscertexpirydate=$(date -d "$certexpirydate" +%s)
-                        echo "Certificate in $line valid until $certexpirydate"
-                        deltadays=$(((tscertexpirydate-tstoday)/86400))
-                        echo "$deltadays days remaining to certificate expiry date"
-                fi
+		month=$(echo $1 | sed 's/notAfter=//' | awk '{print $1}')
+		year=$(echo $1 | sed 's/notAfter=//' | awk '{print $4}')
+		day=$(echo $1 | sed 's/notAfter=//' | awk '{print $2}')
+		case $month in
+			Jan)
+				mn="01"
+				;;
+			Feb)
+				mn="02"
+				;;
+			Mar)
+				mn="03"
+				;;
+			Apr)
+				mn="04"
+				;;
+			May)
+				mn="05"
+				;;
+			Jun)
+				mn="06"
+				;;
+			Jul)
+				mn="07"
+				;;
+			Aug)
+				mn="08"
+				;;
+			Sep)
+				mn="09"
+				;;
+			Oct)
+				mn="10"
+				;;
+			Nov)
+				mn="11"
+				;;
+			Dec)
+				mn="12"
+				;;
+		esac
+		certexpirydate="$year-$mn-$day"
+		tscertexpirydate=$(date -d "$certexpirydate" +%s)
+		echo "Certificate in $line valid until $certexpirydate"
+		deltadays=$(((tscertexpirydate - tstoday) / 86400))
+		minusdeltadays=$(((tstoday - tscertexpirydate) / 86400))
+		if [[ $deltadays -gt 0 ]]; then
+			echo "$deltadays days remaining to certificate expiry date"
+		elif [[ $deltadays -lt 0 ]]; then
+			echo "Certificate has expired $minusdeltadays days ago"
+		fi
+	fi
 }
 
 parse_certificates() {
 	while IFS= read line
 	do
 		if echo "$line" | grep -q "\/"; then
-			opsslcertexpdate=$(echo $line | xargs -i -n 1 openssl x509 -noout -enddate -in {} 2> /dev/null)
+			opsslcertexpdate=$(echo $line | xargs -i -n 1 openssl x509 -noout -enddate -subject -issuer -in {} 2> /dev/null)
 		elif echo "$line" | grep -q "\:"; then
-			opsslcertexpdate=$(echo | openssl s_client -connect "$line" 2>&1 | openssl x509 -noout -enddate 2>/dev/null)
+			opsslcertexpdate=$(echo | openssl s_client -connect "$line" 2>&1 | openssl x509 -noout -enddate -subject -issuer 2>/dev/null)
 		fi
 		if [[ -n "$opsslcertexpdate" ]]; then
+			#echo ""
+			echo "----------------------------------------------------"
+			echo "Certificate Issuer: ${opsslcertexpdate#*issuer=}"
+			#echo $issuer
+			subject=$(echo $opsslcertexpdate | grep -oP '(?<=subject=).*(?=issuer)')
+			echo "Certificate Subject: $subject"
 			parse_date "$opsslcertexpdate"
-			echo ""
 		fi
 	done < $1
 }
